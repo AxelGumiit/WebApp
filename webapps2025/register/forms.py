27e9589d0588cd4e9f8a72d8b02de3e22d17e573
Axel <axelgumiit2@gmail.com>
2025-03-11 16:18:48 +0000

    added the html files

--- webapps2025/register/forms.py
+++ webapps2025/register/forms.py
@@ -1,6 +1,11 @@
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
-class Meta:
-	model = User
-	fields = ["username", "email", "password1", "password2"]
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
