262208a67b23f450ac825ec430e091ee63159b24
Axel <axelgumiit2@gmail.com>
2025-03-12 15:18:59 +0000

    added welcome for the homepage

--- webapps2025/webapps2025/urls.py
+++ webapps2025/webapps2025/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from register.views import register_user 
+from register.views import login_user
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('a', register_user, name='register'),
+    path('', login_user, name='register'),
     path('', include("register.urls")),
     path('', include("payapp.urls"))                     
 
